d21123ea001e0bd7563e0f51a3bd3f471f90a84c
Sakthi Subramaniam <v-saksubramania@expediagroup.com>
2022-05-29 15:09:35 +0200

    Initial commit

--- src/test/java/com/suite/TestSuite.java
+++ src/test/java/com/suite/TestSuite.java
@@ -1,11 +1,13 @@
 package com.suite;
 
 import org.junit.platform.suite.api.ExcludePackages;
+import org.junit.platform.suite.api.IncludePackages;
 import org.junit.platform.suite.api.SelectPackages;
 import org.junit.platform.suite.api.Suite;
 
 @Suite
 @SelectPackages("com")
-@ExcludePackages("com.suite")
+//@ExcludePackages("com.suite")
+@IncludePackages("com.junit5")
 public class TestSuite {
 }
